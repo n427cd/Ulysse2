3c10caed4c46282c9cefa2237ce5be472c807b55
Eric Duchenne <duchenne.eric@wanadoo.fr>
2021-07-13 15:05:12 +0200

    Extraction de la classe `SelectedIndex`

--- Ulysse2/Ulysse/Views/Cartographie/ScannedMapView.swift
+++ Ulysse2/Ulysse/Views/Cartographie/ScannedMapView.swift
@@ -12,23 +12,6 @@ import Foundation
 import UIKit
 
 
-/// Encapsulation des index pour la sélection des waypoints ou des segments
-///
-/// Autorise la modification des index dans la vue
-/// - Remark : Permet de corriger le hack qui consistait à passer par une variable statique
-
-class SelectedIndex {
-   /// index du waypoint sélectionné. `nil` si pas de sélection active
-   var waypoint : Int?
-
-   /// index du segment sélectionné. `nil` si pas de sélection active
-   var segment : Int?
-
-   /// Initialisation : pas de sélection par défaut
-   init() {}
-}
-
-
 /// Vue permettant la manipulation de cartes scannées
 /// - TODO: #2  Implémentation du multizoom
 /// - TODO: #3 implémentation de la suppression d'un Waypoint
